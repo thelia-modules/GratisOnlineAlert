2b8ae788df0b163c29e279e25d58dce4afce2744
Romain Ducher <rducher@openstudio.fr>
2015-06-05 10:50:10 +0200

    I18n FTW!

---
+++ I18n/email/default/en_US.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'free_product %title' => 'The product %title is free',
+    'click %link' => 'Click <a href=\"%link\">here</a> to see it',
+    'link %link' => 'Link: %link',
+    'ref %ref' => 'Ref: %ref',
+);
